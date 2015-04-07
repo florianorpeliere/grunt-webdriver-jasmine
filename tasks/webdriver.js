b5c3d67467fd088fc8c0d4800960118e969dbf2a
Florian Orpelière <florian.orpeliere@gmail.com>
2015-04-07 17:09:53 +0200

    Fix problem with callback()

--- tasks/webdriver.js
+++ tasks/webdriver.js
@@ -1,23 +1,23 @@
-var SauceLabs   = require('saucelabs'),
+var SauceLabs = require('saucelabs'),
     SauceTunnel = require('sauce-tunnel'),
-    selenium    = require('selenium-standalone'),
+    selenium = require('selenium-standalone'),
     webdriverjs = require('webdriverjs'),
-    util        = require('util'),
-    http        = require('http'),
-    async       = require('async'),
-    hooker      = require('hooker'),
-    path        = require('path'),
-    fs          = require('fs-extra'),
-    deepmerge   = require('deepmerge'),
-    server      = null,
-    tunnel      = null,
-    isHookedUp  = false,
+    util = require('util'),
+    http = require('http'),
+    async = require('async'),
+    hooker = require('hooker'),
+    path = require('path'),
+    fs = require('fs-extra'),
+    deepmerge = require('deepmerge'),
+    server = null,
+    tunnel = null,
+    isHookedUp = false,
     isSauceTunnelRunning = false,
     isSeleniumServerRunning = false;
 
 module.exports = function(grunt) {
 
-    grunt.registerMultiTask('webdriver_jasmine', 'run WebdriverJS jasmine tests', function() {
+   grunt.registerMultiTask('webdriver_jasmine', 'run WebdriverJS jasmine tests', function () {
 
         var that = this,
             done = this.async(),
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
                     consolidate: true
                 }
             }),
-            capabilities = deepmerge(options,this.data.options || {}),
+            capabilities = deepmerge(options, this.data.options || {}),
             tunnelIdentifier = options['tunnel-identifier'] || (capabilities.desiredCapabilities ? capabilities.desiredCapabilities['tunnel-identifier'] : null) || null,
             tunnelFlags = (capabilities.desiredCapabilities ? capabilities.desiredCapabilities['tunnel-flags'] : []) || [],
             isLastTask = grunt.task._queue.length - 2 === 0,
@@ -70,10 +70,10 @@ module.exports = function(grunt) {
         if (options.projectRoot) {
             options.specFolders.push(options.projectRoot);
         }
-        var regExpSpec = new RegExp(options.match + (options.matchall ? "" : options.specNameMatcher + "\\." ) + "(" + options.extensions + ")$", 'i');
+        var regExpSpec = new RegExp(options.match + (options.matchall ? "" : options.specNameMatcher + "\\.") + "(" + options.extensions + ")$", 'i');
 
         if (options.useHelpers) {
-            this.filesSrc.forEach(function(path) {
+            this.filesSrc.forEach(function (path) {
                 testSuite.loadHelpersInFolder(path, new RegExp(options.helperNameMatcher + "?\\.(" + options.extensions + ")$", 'i'));
             });
         }
@@ -91,7 +91,7 @@ module.exports = function(grunt) {
          * hook process.stdout.write to save reporter output into file
          * thanks to https://github.com/pghalliday/grunt-mocha-test
          */
-        if(!isHookedUp) {
+        if (!isHookedUp) {
             if (options.output) {
                 fs.mkdirsSync(path.dirname(options.output));
                 fd = fs.openSync(options.output, 'w');
@@ -101,7 +101,7 @@ module.exports = function(grunt) {
             hooker.hook(process.stdout, 'write', {
 
                 // This gets executed before the original process.stdout.write
-                pre: function(result) {
+                pre: function (result) {
 
                     // Write result to file if it was opened
                     if (fd) {
@@ -128,15 +128,15 @@ module.exports = function(grunt) {
         var uncaughtExceptionHandlers = process.listeners('uncaughtException');
         process.removeAllListeners('uncaughtException');
 
-        var unmanageExceptions = function() {
+        var unmanageExceptions = function () {
             uncaughtExceptionHandlers.forEach(process.on.bind(process, 'uncaughtException'));
         };
 
         /**
          * initialise tunnel
          */
-        if(!tunnel && options.user && options.key && tunnelIdentifier) {
-            tunnel = new SauceTunnel(options.user , options.key, tunnelIdentifier, true, tunnelFlags);
+        if (!tunnel && options.user && options.key && tunnelIdentifier) {
+            tunnel = new SauceTunnel(options.user, options.key, tunnelIdentifier, true, tunnelFlags);
             tunnel.on('verbose:debug', grunt.log.debug);
         }
 
@@ -148,7 +148,7 @@ module.exports = function(grunt) {
         /**
          * helper function for asyncjs
          */
-        var next = function() {
+        var next = function () {
             this.call(null, null, Array.prototype.slice.call(arguments)[0]);
         }
 
@@ -157,9 +157,9 @@ module.exports = function(grunt) {
             /**
              * check if selenium server is already running
              */
-            function(callback) {
+            function (callback) {
 
-                if(tunnel) {
+                if (tunnel) {
                     return callback();
                 }
 
@@ -169,11 +169,11 @@ module.exports = function(grunt) {
                     path: '/wd/hub/status'
                 };
 
-                http.get(options, function() {
+                http.get(options, function () {
                     isSeleniumServerRunning = true;
-                    callback();
-                }).on('error', function() {
-                    callback();
+                    callback(null);
+                }).on('error', function () {
+                    callback(null);
                 });
 
             },
@@ -181,12 +181,12 @@ module.exports = function(grunt) {
             /**
              * start selenium server or sauce tunnel (if not already started)
              */
-            function(callback) {
+            function (callback) {
 
-                if(tunnel) {
+                if (tunnel) {
 
-                    if(isSauceTunnelRunning) {
-                        return callback(null,true);
+                    if (isSauceTunnelRunning) {
+                        return callback(null, true);
                     }
 
                     grunt.log.debug('start sauce tunnel');
@@ -196,23 +196,29 @@ module.exports = function(grunt) {
                      */
                     tunnel.start(next.bind(callback));
 
-                } else if(!server && !isSeleniumServerRunning && !options.nospawn) {
+                }
+                else if (!server && !isSeleniumServerRunning && !options.nospawn) {
 
                     grunt.log.debug('start selenium standalone server');
 
                     /**
                      * starts selenium standalone server if its not running
                      */
-                    server = selenium.start({ stdio: 'pipe' });
+                    server = selenium.start({
+                        stdio: 'pipe'
+                    });
 
                     /**
                      * listen on server output
                      */
-                    server.stdout.on('data', next.bind(callback));
+                    ['stderr', 'stdout'].forEach(function (output) {
+                        server[output].on('data', next.bind(callback));
+                    });
 
-                } else {
+                }
+                else {
 
-                    callback(null,true);
+                    callback(null, true);
 
                 }
 
@@ -221,20 +227,21 @@ module.exports = function(grunt) {
             /**
              * check if server is ready
              */
-            function(output,callback) {
+            function (output, callback) {
 
-                if(tunnel && !isSauceTunnelRunning) {
+                if (tunnel && !isSauceTunnelRunning) {
 
                     // output here means if tunnel was created successfully
-                    if(output === false) {
+                    if (output === false) {
                         grunt.fail.warn(new Error('Sauce-Tunnel couldn\'t created successfully'));
                     }
 
                     grunt.log.debug('tunnel created successfully');
                     isSauceTunnelRunning = true;
-                    callback();
+                    callback(null);
 
-                } else if(server && !isSeleniumServerRunning && !options.nospawn) {
+                }
+                else if (server && !isSeleniumServerRunning && !options.nospawn) {
 
                     var line = output.toString().trim();
 
@@ -242,13 +249,18 @@ module.exports = function(grunt) {
                     if (line && line.indexOf('Started HttpContext[/wd,/wd]') > -1) {
                         grunt.log.debug('selenium server started successfully');
                         isSeleniumServerRunning = true;
-                        server.stdout.removeAllListeners('data');
-                        callback();
+
+                        ['stderr', 'stdout'].forEach(function (output) {
+                            server[output].removeAllListeners('data');
+                        });
+
+                        callback(null);
                     }
 
-                } else {
+                }
+                else {
 
-                    callback();
+                    callback(null);
 
                 }
 
@@ -257,7 +269,7 @@ module.exports = function(grunt) {
             /**
              * init WebdriverJS instance
              */
-            function(callback) {
+            function (callback) {
                 grunt.log.debug('init WebdriverJS instance');
 
                 GLOBAL.browser.init().call(next.bind(callback));
@@ -266,12 +278,12 @@ module.exports = function(grunt) {
             /**
              * run tests
              */
-            function(args,callback) {
+            function (args, callback) {
                 grunt.log.debug('run tests with jasmine');
 
                 // jasmine
-                var onJasmineComplete = function(runner, log) {
-                    testSuite.getGlobal( ).jasmine.currentEnv_ = undefined;
+                var onJasmineComplete = function (runner, log) {
+                    testSuite.getGlobal().jasmine.currentEnv_ = undefined;
                     callback(null, runner.results().failedCount);
                 };
 
@@ -280,36 +292,37 @@ module.exports = function(grunt) {
                 // for jasmine-node 1.3
                 testSuite.executeSpecsInFolder(jasmineOptions);
                 // for jasmine-node 2
-                // jasmineOptions.watchFolders = [ ];
-                // testSuite.run( jasmineOptions );
+                //jasmineOptions.watchFolders = [ ];
+                //testSuite.run( jasmineOptions );
             },
 
             /**
              * handle test results
              */
-            function(args,callback) {
+            function (args, callback) {
                 grunt.log.debug('handle test results');
 
                 // Restore grunt exception handling
                 unmanageExceptions();
 
                 // Close Remote sessions if needed
-                GLOBAL.browser.end(next.bind(callback,args));
+                GLOBAL.browser.end(next.bind(callback, args));
             },
 
             /**
              * destroy sauce tunnel if connected (once all tasks were executed)
              */
-            function(args,callback) {
+            function (args, callback) {
 
-                if(isLastTask && isSauceTunnelRunning) {
+                if (isLastTask && isSauceTunnelRunning) {
 
                     grunt.log.debug('destroy sauce tunnel if connected (once all tasks were executed)');
-                    tunnel.stop(next.bind(callback,args));
+                    tunnel.stop(next.bind(callback, args));
 
-                } else {
+                }
+                else {
 
-                    callback(null,args);
+                    callback(null, args);
 
                 }
 
@@ -318,10 +331,10 @@ module.exports = function(grunt) {
             /**
              * update job on Sauce Labs
              */
-            function(args,callback) {
+            function (args, callback) {
                 grunt.log.debug('update job on Sauce Labs');
 
-                if(options.user && options.key && options.updateSauceJob) {
+                if (options.user && options.key && options.updateSauceJob) {
 
                     var sauceAccount = new SauceLabs({
                         username: options.user,
@@ -331,11 +344,12 @@ module.exports = function(grunt) {
                     sauceAccount.updateJob(GLOBAL.browser.requestHandler.sessionID, {
                         passed: args === 0,
                         public: true
-                    }, next.bind(callback,args === 0));
+                    }, next.bind(callback, args === 0));
 
-                } else {
+                }
+                else {
 
-                    callback(null,args === 0);
+                    callback(null, args === 0);
 
                 }
 
@@ -344,13 +358,10 @@ module.exports = function(grunt) {
             /**
              * finish grunt task
              */
-            function(args,callback){
-                grunt.log.debug('finish grunt task',args);
+            function (args, callback) {
+                grunt.log.debug('finish grunt task', args);
 
-                done(args);
-                callback();
-
-                if(isLastTask) {
+                if (isLastTask) {
 
                     // close the file if it was opened
                     if (fd) {
@@ -361,6 +372,9 @@ module.exports = function(grunt) {
                     hooker.unhook(process.stdout, 'write');
 
                 }
+
+                done(args);
+                callback();
             }
         ]);
 
